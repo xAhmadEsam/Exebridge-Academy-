9ead56f865efe28b347f5852acb27632a996d406
AhmadEsam <79665284+xAhmadEsam@users.noreply.github.com>
2025-11-06 18:47:48 +0300

    Add files via upload

--- DataBase/CoursesData.js
+++ DataBase/CoursesData.js
@@ -1,31 +1,31 @@
 export const courses = [
     {
         name: 'Italian',
-        mainImg: '../Assets/Imgs/Italian/1.jpg',
+        mainImg: 'Assets/Imgs/Italian/1.jpg',
         mainText: 'For what reason would it be advisable for me to think about business content?',
         filterTags: ['Italian cuisine', 'Cuisine Type']
     },
     {
         name: 'Middle east',
-        mainImg: '../Assets/Imgs/Middle-East/1.jpg',
+        mainImg: 'Assets/Imgs/Middle-East/1.jpg',
         mainText: 'For what reason would it be advisable for me to think about business content?',
         filterTags: ['Middle east cuisine', 'Cuisine Type']
     },
     {
         name: 'Asian',
-        mainImg: '../Assets/Imgs/Asian/1.jpg',
+        mainImg: 'Assets/Imgs/Asian/1.jpg',
         mainText: 'For what reason would it be advisable for me to think about business content?',
         filterTags: ['Asian cuisine', 'Cuisine Type']
     },
     {
         name: 'Innovative dishes',
-        mainImg: '../Assets/Imgs/Innovativedishes/1.jpg',
+        mainImg: 'Assets/Imgs/Innovativedishes/1.jpg',
         mainText: 'For what reason would it be advisable for me to think about business content?',
         filterTags: ['Innovative dishes', 'Dish Type', 'Techniques & Styles']
     },
     {
         name: 'Stack',
-        mainImg: '../Assets/Imgs/Stack/1.jpg',
+        mainImg: 'Assets/Imgs/Stack/1.jpg',
         mainText: 'For what reason would it be advisable for me to think about business content?',
         filterTags: ['Steak', 'Dish Type']
     },
@@ -36,7 +36,7 @@ export const courses = [
     },
     {
         name: 'Croisant',
-        mainImg: '../Assets/Imgs/Croissant/1.jpg',
+        mainImg: 'Assets/Imgs/Croissant/1.jpg',
         mainText: 'For what reason would it be advisable for me to think about business content?',
         filterTags: ['Croissant', 'Dish Type']
     },
@@ -47,7 +47,7 @@ export const courses = [
     },
     {
         name: 'Pizza',
-        mainImg: '../Assets/Imgs/Pizza/1.jpg',
+        mainImg: 'Assets/Imgs/Pizza/1.jpg',
         mainText: 'For what reason would it be advisable for me to think about business content?',
         filterTags: ['Pizza', 'Dish Type']
     },
@@ -58,13 +58,13 @@ export const courses = [
     },
     {
         name: 'Cake',
-        mainImg: '../Assets/Imgs/Cake/1.jpg',
+        mainImg: 'Assets/Imgs/Cake/1.jpg',
         mainText: 'For what reason would it be advisable for me to think about business content?',
         filterTags: ['Cakes', 'Desserts & Sweets']
     },
     {
         name: 'Innovative middle eastern deserts',
-        mainImg: '../Assets/Imgs/Innovativemiddleeasterndeserts/1.jpg',
+        mainImg: 'Assets/Imgs/Innovativemiddleeasterndeserts/1.jpg',
         mainText: 'For what reason would it be advisable for me to think about business content?',
         filterTags: ['Innovative middle eastern deserts', 'Desserts & Sweets']
     },
@@ -75,7 +75,7 @@ export const courses = [
     },
     {
         name: 'Isomalt sugar art',
-        mainImg: '../Assets/Imgs/Isomaltsugarart/1.jpg',
+        mainImg: 'Assets/Imgs/Isomaltsugarart/1.jpg',
         mainText: 'For what reason would it be advisable for me to think about business content?',
         filterTags: ['Isomalt sugar art', 'Desserts & Sweets', 'Techniques & Styles']
     },
@@ -86,13 +86,13 @@ export const courses = [
     },
     {
         name: 'Fruit & vegetables carving',
-        mainImg: '../Assets/Imgs/Fruit&vegetablescarving/1.jpg',
+        mainImg: 'Assets/Imgs/Fruit&vegetablescarving/1.jpg',
         mainText: 'For what reason would it be advisable for me to think about business content?',
         filterTags: ['Techniques & Styles']
     },
     {
         name: 'French cuisine',
-        mainImg: '../Assets/Imgs/Frenchcuisine/1.jpg',
+        mainImg: 'Assets/Imgs/Frenchcuisine/1.jpg',
         mainText: 'For what reason would it be advisable for me to think about business content?',
         filterTags: ['French cuisine', 'Cuisine Type']
     }

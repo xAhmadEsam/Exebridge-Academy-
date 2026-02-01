6e67badccff066fb77c28319a7e5ad727652fd55
AhmadEsam <79665284+xAhmadEsam@users.noreply.github.com>
2026-02-01 16:56:35 +0300

    Add files via upload

--- DataBase/CoursesData.js
+++ DataBase/CoursesData.js
@@ -9,91 +9,91 @@ export const courses = [
     {
         name: 'Italian',
         mainImg: 'Assets/Imgs/Italian/1.jpeg',
-        mainText: 'For what reason would it be advisable for me to think about business content?',
+        mainText: 'An Italian kitchen where tradition meets creativity...',
         filterTags: ['Italian cuisine', 'Cuisine Type'],
         link: 'Courses/Italian.html'
     },
     {
         name: 'Middle east',
         mainImg: 'Assets/Imgs/Middle-East/1.jpg',
-        mainText: 'For what reason would it be advisable for me to think about business content?',
+        mainText: 'mmerse yourself in the flavors of the Middle East and the magic of Arabic cuisine...',
         filterTags: ['Middle east cuisine', 'Cuisine Type'],
         link: 'Courses/MiddleEast.html'
     },
     {
         name: 'Asian',
         mainImg: 'Assets/Imgs/Asian/1.jpeg',
-        mainText: 'For what reason would it be advisable for me to think about business content?',
+        mainText: 'Explore the diverse and vibrant flavors of Asian cuisine...',
         filterTags: ['Asian cuisine', 'Cuisine Type'],
         link: 'Courses/Asian.html'
     },
     {
         name: 'Innovative dishes',
         mainImg: 'Assets/Imgs/Innovativedishes/1.jpg',
-        mainText: 'For what reason would it be advisable for me to think about business content?',
+        mainText: 'Embark on a creative journey to master the art of innovation in the kitchen!...',
         filterTags: ['Innovative dishes', 'Dish Type', 'Techniques & Styles'],
         link: 'Courses/InnovativeDishes.html'
     },
     {
         name: 'Steak',
         mainImg: 'Assets/Imgs/Steak/1.jpeg',
-        mainText: 'For what reason would it be advisable for me to think about business content?',
+        mainText: 'Master the art of preparing and cooking premium steaks with perfect texture and flavor...',
         filterTags: ['Steak', 'Dish Type'],
         link: 'Courses/Steak.html'
     },
     {
         name: 'Croisant',
         mainImg: 'Assets/Imgs/Croisant/1.jpg',
-        mainText: 'For what reason would it be advisable for me to think about business content?',
+        mainText: 'This one-week course is dedicated to mastering the art of authentic French croissant making...',
         filterTags: ['Croissant', 'Dish Type'],
         link: 'Courses/Croisant.html'
     },
     {
         name: 'Pizza',
         mainImg: 'Assets/Imgs/Pizza/1.jpeg',
-        mainText: 'For what reason would it be advisable for me to think about business content?',
+        mainText: 'Discover the secrets to crafting authentic Italian pizza from scratch...',
         filterTags: ['Pizza', 'Dish Type'],
         link: 'Courses/Pizza.html'
     },
     {
         name: 'Chocolate',
         mainImg: 'Assets/Imgs/Choclet/5.jpg',
-        mainText: 'For what reason would it be advisable for me to think about business content?',
+        mainText: 'Master the art of creating exquisite chocolate desserts and confections with our comprehensive chocolate course...',
         filterTags: ['Chocolate', 'Desserts & Sweets'],
         link: 'Courses/Chocolate.html'
     },
     {
         name: 'Cake',
         mainImg: 'Assets/Imgs/Cake/1.jpg',
-        mainText: 'For what reason would it be advisable for me to think about business content?',
+        mainText: 'Learn the art of creating beautiful and delicious cakes with our comprehensive cake-making course...',
         filterTags: ['Cakes', 'Desserts & Sweets'],
         link: 'Courses/Cake.html'
     },
     {
         name: 'Innovative middle eastern deserts',
         mainImg: 'Assets/Imgs/Innovativemiddleeasterndeserts/1.jpg',
-        mainText: 'For what reason would it be advisable for me to think about business content?',
+        mainText: 'Discover the innovative and creative world of Middle Eastern desserts with our specialized course...',
         filterTags: ['Innovative middle eastern deserts', 'Desserts & Sweets'],
         link: 'Courses/MiddleEastDessert.html'
     },
     {
         name: 'Isomalt sugar art',
         mainImg: 'Assets/Imgs/Isomaltsugarart/1.jpg',
-        mainText: 'For what reason would it be advisable for me to think about business content?',
+        mainText: 'Master the art of creating stunning sugar sculptures and decorative elements with isomalt...',
         filterTags: ['Isomalt sugar art', 'Desserts & Sweets', 'Techniques & Styles'],
         link: 'Courses/SugarArt.html'
     },
     {
         name: 'Fruit & vegetables carving',
         mainImg: 'Assets/Imgs/Fruit%26vegetablescarving/1.jpg',
-        mainText: 'For what reason would it be advisable for me to think about business content?',
+        mainText: 'Learn the art of creating beautiful and intricate fruit and vegetable sculptures with our specialized course...',
         filterTags: ['Techniques & Styles'],
             link: 'Courses/F-V-Carving.html'
     },
     {
         name: 'French cuisine',
         mainImg: 'Assets/Imgs/Frenchcuisine/1.jpg',
-        mainText: 'For what reason would it be advisable for me to think about business content?',
+        mainText: 'Discover the rich traditions and refined techniques of French culinary arts...',
         filterTags: ['French cuisine', 'Cuisine Type'],
         link: 'Courses/FrenchCusine.html'
     }
